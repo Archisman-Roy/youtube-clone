2c26ffa8cde351f1fb24b45325c52367145d4c56
Archisman-Roy <royarchi31@gmail.com>
2023-08-16 09:11:04 -0700

    added delete and upload functions

--- video-processing-service/src/storage.ts
+++ video-processing-service/src/storage.ts
@@ -29,3 +29,44 @@ export function convertVideo(rawVideoName: string, processedVideoName: string) {
       .save(`${localProcessedVideoPath}/${processedVideoName}`);
     }) 
 }
+
+export async function downloadRawVideo(fileName: string) {
+
+  await storage.bucket(rawVideoBucketName)
+  .file(fileName)
+  .download({ destination:`${localRawVideoPath}/${fileName}`});
+
+  console.log(`gs://${rawVideoBucketName}/${fileName} downloaded to ${localRawVideoPath}/${fileName}.`);
+}
+
+export async function uploadProcessedVideo(fileName: string) {
+  const bucket = storage.bucket(processedVideoBucketName);
+  
+  await bucket.upload(`${localProcessedVideoPath}/${fileName}`, {
+    destination: fileName
+  });
+
+  console.log(`gs://${localProcessedVideoPath}/${fileName} uploaded to ${processedVideoBucketName}/${fileName}.`);
+
+  await bucket.file(fileName).makePublic();
+  
+}
+
+function deleteFile(filePath: string): Promise<void> {
+  return new Promise((resolve,reject)=> {
+    if(fs.existsSync(filePath)) {
+      fs.unlink(filePath, (err) => {
+        if(err) {
+          console.log(`Failed to delete file at ${filePath}`);
+          console.log(JSON.stringify(err));
+          reject(err);
+        } else {
+          console.log(`File deleted at ${filePath}`);
+        }
+      })
+    } else {
+      console.log(`File was not found at ${filePath}, skipping the  delete`);
+      resolve();
+    }
+  });
+}
